cb93307f205947b0563dfb3989145a9c0c980564
linse2268 <161201558+linse2268@users.noreply.github.com>
2024-10-04 09:58:52 +0200

    hoverFarbe im Lightmode gefixt

--- script.js
+++ script.js
@@ -18,14 +18,8 @@ function lightDark(){
         document.documentElement.style.setProperty('--fontcolor', 'rgb(29, 29, 29)');
         document.documentElement.style.setProperty('--bodybackgroundcolor', 'rgb(185, 185, 185)');
         document.documentElement.style.setProperty('--linsenPodcolor', 'rgba(29, 29, 29, 0.75)');
-        document.documentElement.style.setProperty('--backgroundcolorHover', 'rgb(66, 66, 66)');
+        document.documentElement.style.setProperty('--backgroundcolorHover', 'rgb(219, 219, 219)');
         dark = false;
         lightDarkButton.innerText = "switch to darkmode";
     }
 }
-
-
-//--backgroundcolor: rgb(202, 202, 202)
-
-
-
